563e209abcd4e615f966826d6e2bceee10564e4c
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2018-11-17 15:12:46 -0500

    up to part 2 done

--- bayes_template.py
+++ bayes_template.py
@@ -2,18 +2,54 @@ import math, os, pickle, re
 
 class Bayes_Classifier:
 
+   self.GoodWords
+   self.BadWords
+
    def __init__(self, trainDirectory = "movie_reviews/"):
-      '''This method initializes and trains the Naive Bayes Sentiment Classifier.  If a 
-      cache of a trained classifier has been stored, it loads this cache.  Otherwise, 
-      the system will proceed through training.  After running this method, the classifier 
+      '''self method initializes and trains the Naive Bayes Sentiment Classifier.  If a 
+      cache of a trained classifier has been stored, it loads self cache.  Otherwise, 
+      the system will proceed through training.  After running self method, the classifier 
       is ready to classify input text.'''
+      if os.path.exists('good.txt'):
+         self.GoodWords = self.load('good.txt')
+         self.BadWords = self.load('bad.txt')
+      else:
+         self.train()
+
 
    def train(self):   
       '''Trains the Naive Bayes Sentiment Classifier.'''
+      IFileList = []
+      self.GoodWords = {}
+      self.BadWords = {}
+      for fFileObj in os.walk('movies_reviews/'):
+         IFileList = fFileObj[2]
+         break
+      for ifl in IFileList:
+         full_text = self.loadFile('movies_reviews/'+ifl)
+         text = self.tokenize(full_text)
+         if  re.match('^movies-1-\d+.txt$', ifl):
+            for t in text:
+               if t in BadWords.keys():
+                  BadWords[t]+=1
+               else:
+                  BadWords[t] = 1
+            continue
+         if re.match('^movies-5-\d+.txt$', ifl):
+            for t in text:
+               if t in GoodWords.keys():
+                  GoodWords[t]+=1
+               else:
+                  GoodWords[t] = 1
+      self.save(GoodWords,'good.txt')
+      self.save(BadWords,'bad.txt')
+
+      
+      
     
    def classify(self, sText):
-      '''Given a target string sText, this function returns the most likely document
-      class to which the target string belongs. This function should return one of three
+      '''Given a target string sText, self function returns the most likely document
+      class to which the target string belongs. self function should return one of three
       strings: "positive", "negative" or "neutral".
       '''
 
@@ -28,15 +64,14 @@ class Bayes_Classifier:
    def save(self, dObj, sFilename):
       '''Given an object and a file name, write the object to the file using pickle.'''
 
-      f = open(sFilename, "w")
+      f = open(sFilename, "wb")
       p = pickle.Pickler(f)
       p.dump(dObj)
       f.close()
    
    def load(self, sFilename):
       '''Given a file name, load and return the object stored in the file.'''
-
-      f = open(sFilename, "r")
+      f = open(sFilename, "rb")
       u = pickle.Unpickler(f)
       dObj = u.load()
       f.close()
---
+++ test.py
@@ -0,0 +1,6 @@
+from bayes_template import Bayes_Classifier
+
+bc = Bayes_Classifier()
+#
+'''dObj = bc.loadFile('movies_reviews/movies-1-1077.txt')
+print(bc.tokenize(dObj))'''
