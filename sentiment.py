efeb4b94bbd859259cce35be46e30c5e330bead6
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2018-11-24 14:01:45 -0500

    add optional params

--- sentiment.py
+++ sentiment.py
@@ -1,7 +1,18 @@
 import math, os, pickle, re,argparse,time
 
+def remove_training_files():
+    try:
+        os.remove('good.txt')
+        os.remove('bad.txt')
+        os.remove('bi_bad.txt')
+        os.remove('bi_good.txt')
+    except:
+        print('An exception was raise while removing files.\nThat is ok.')
+        return True
+    return True
+    
 
-def main(test_files,best):
+def main(test_files,best,bigram,rows):
     print('remember to train the bayes')
     if(best):
         execfile('bayes_best.py')
@@ -12,17 +23,19 @@ def main(test_files,best):
     for fFileObj in os.walk(test_files ):
         iFileList = fFileObj[2]
         break
-    print('%d test reviews.', len(iFileList))
     results = {"negative":0, "neutral":0, "positive":0}
     print("\nFile Classifications:")
     t = time.time()
-
     for filename in iFileList:
         fileText = bc.loadFile(test_files + filename)
-        result = bc.classify(fileText)
-        te = time.time() - t
-        print(filename,result,'%.2f seconds'%te)
-        t = time.time()
+        if bigram:
+            result = bc.classify_bi(fileText)
+        else:
+            result = bc.classify(fileText)
+        if rows:
+            te = time.time() - t
+            print(filename,result,'%.2f seconds'%te)
+            t = time.time()
         results[result] += 1
 
     print("\nResults Summary:")
@@ -34,6 +47,17 @@ def main(test_files,best):
 if __name__ == '__main__':
     PARSER = argparse.ArgumentParser()
     PARSER.add_argument('testfiles',help='takes the path of a directory of text files',type = str)
-    PARSER.add_argument('best',help = 'use the best bay', type = bool)
+    group = PARSER.add_argument_group()
+    group.add_argument('-b','--best',action='store_true',help='use the best')
+    group.add_argument('-bi','--bigram',action='store_true',help='use the bigram')
+    group.add_argument('-t','--train',action='store_true',help='use to train the program')
+    group.add_argument('-s','--showrows',action='store_true',help='show results')
+
     ARGS = PARSER.parse_args()
     main(ARGS.testfiles,ARGS.best)
+    if ARGS.train:
+        remove_training_files()
+    if ARGS.bigram is True and ARGS.best is False:
+        raise ValueError('you cannot call bigragm option with the best option')
+    else:
+        main(ARGS.test_files,ARGS.best,ARGS.bigram,ARGS.showrows)
